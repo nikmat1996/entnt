50fcaf9d72f33642e1bc9297dbef01cabfcf734e
nikmat1996 <nikhilmathew1996@gmail.com>
2023-12-02 02:01:25 +0530

    init

---
+++ app/components/locationIcon.jsx
@@ -0,0 +1,16 @@
+export default () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="h-4"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 2c-3.04 0-5.5 2.58-5.5 5.75C6.5 11.17 12 20 12 20s5.5-8.83 5.5-11.25C17.5 4.58 15.04 2 12 2zM12 9a2.5 2.5 0 100-5 2.5 2.5 0 000 5z"
+        />
+    </svg>
+);
